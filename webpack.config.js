d073bb7673647874c006ef165b284488319088ff
houfio <26674115+houfio@users.noreply.github.com>
2020-03-10 10:42:09 +0100

    Add webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    inline: true
+  }
+};
