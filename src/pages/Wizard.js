0380fe3fbfc1a327033fcda05e44b4334dcc4638
Raul de Heer <raul@supertask.nl>
2020-03-10 15:24:12 +0100

    Fix merge conflicts

--- src/pages/Wizard.js
+++ src/pages/Wizard.js
@@ -6,32 +6,95 @@ export class Wizard extends Page {
       ...state,
       step: 0,
       inputData: {
-        name: "Naam",
-        description: "Beschrijving",
-        purchasePrice: "Inkoopprijs",
-        price: "Verkoopprijs",
-        stock: "Voorraad",
-        minimum_stock: "Minimum voorraad",
+        name: ['Naam'],
+        description: ['Beschrijving'],
+        purchasePrice: ['Inkoopprijs'],
+        price: ['Verkoopprijs'],
+        stock: ['Voorraad'],
+        minimum_stock: ['Minimum voorraad'],
       }
     }, rerender);
   }
 
   render() {
+    const id = Object.keys(this.state.inputData)[this.state.step];
+    const label = Object.values(this.state.inputData)[this.state.step][0];
+
     return `
       <div class="form-group">
-        <label for="${Object.keys(this.state.inputData)[this.state.step]}">${Object.values(this.state.inputData)[this.state.step]}</label>
-        <input type="text" class="form-control" id="${Object.keys(this.state.inputData)[this.state.step]}">
+        <label for="${id}">${label}</label>
+        <input type="text" class="form-control" id="${id}">
       </div>
       <button onclick="nextStep" class="btn btn-primary">Volgende stap</button>
     `;
   }
 
-  nextStep = (element) => {
-    console.log(element);
+  mount() {
+    let extraFields = [];
+
+    switch (this.state.name) {
+      case 'decoration':
+        extraFields = [
+          {
+            key: 'size',
+            label: 'Grootte'
+          },
+          {
+            key: 'color',
+            label: 'Kleur'
+          },
+          {
+            key: 'package_amount',
+            label: 'Aantal per verpakking'
+          },
+        ];
+        break;
+      case 'tierlantin':
+        extraFields = [
+          {
+            key: 'weight',
+            label: 'Gewicht'
+          }
+        ];
+        break;
+      default:
+        extraFields = [
+          {
+            key: 'color',
+            label: 'Kleur'
+          },
+          {
+            key: 'size',
+            label: 'Grootte'
+          },
+        ];
+        break;
+    }
+
+    extraFields.map(extraField => {
+      this.set({
+        ...this.state,
+        inputData: {
+          ...this.state.inputData,
+          [extraField.key]: [extraField.label]
+        }
+      });
+    });
+  }
+
+  nextStep = () => {
+    const id = Object.keys(this.state.inputData)[this.state.step];
 
     this.set({
       ...this.state,
-      step: this.state.step + 1
+      step: this.state.step + 1,
+      inputData: {
+        ...this.state.inputData,
+        [id]: [
+          ...this.state.inputData[id],
+          document.getElementById(id).value
+        ]
+      }
     });
   };
 }
