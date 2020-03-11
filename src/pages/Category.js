d95e53b9f54dfda8ac44dc578cd5779d2a28b4a0
houfio <26674115+houfio@users.noreply.github.com>
2020-03-11 22:58:43 +0100

    Display products on category page

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,14 +1,24 @@
 import { Page } from '../Page';
 import { html } from '../utils/html';
 import { navigate } from '../utils/navigate';
+import { storage } from '../utils/storage';
+import { Product } from '../models/Product';
 
 export class Category extends Page {
   render() {
+    const { name } = this.state;
+    const products = storage.get(name, Product);
+
     return html`
       <div class="container">
         <button class="btn btn-primary" onclick="navigateTo">
           Product toevoegen
         </button>
+        ${products.map((product) => html`
+          <div>
+            Product: ${product.name}
+          </div>
+        `)}
       </div>
     `;
   }
