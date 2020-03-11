1a4052e5feb547756f16118126050fe27858c3b5
houfio <26674115+houfio@users.noreply.github.com>
2020-03-11 22:57:55 +0100

    Improve state update perf

--- src/Page.js
+++ src/Page.js
@@ -1,9 +1,15 @@
+import equal from 'fast-deep-equal/es6';
+
 export class Page {
   #rerender;
 
-  constructor(state, rerender) {
-    this.state = state;
+  constructor(params, rerender) {
+    this.state = this.initial(params);
     this.#rerender = rerender;
+  }
+
+  initial(params) {
+    return params;
   }
 
   render() {
@@ -13,16 +19,15 @@ export class Page {
   mount() {
   }
 
-  update() {
-  }
-
   unmount() {
   }
 
   set(state, mandatory = true) {
+    const current = this.state;
+
     this.state = state;
 
-    if (mandatory) {
+    if (mandatory && !equal(current, this.state)) {
       this.#rerender();
     }
   }
