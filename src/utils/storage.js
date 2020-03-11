dcee55b73b7a3b4b60dbff7bec7775af4165070c
houfio <26674115+houfio@users.noreply.github.com>
2020-03-11 22:57:16 +0100

    Add storage utility

---
+++ src/utils/storage.js
@@ -0,0 +1,37 @@
+class Storage {
+  #getRaw(key) {
+    try {
+      const result = JSON.parse(localStorage.getItem(key));
+      console.log(result);
+
+      if (!Array.isArray(result)) {
+        return [];
+      }
+
+      return result;
+    } catch {
+      return [];
+    }
+  }
+
+  push(key, obj) {
+    let list = this.#getRaw(key);
+
+    localStorage.setItem(key, JSON.stringify([
+      ...list,
+      obj
+    ]));
+  }
+
+  get(key, cls) {
+    return this.#getRaw(key).map((data) => Object.assign(new cls, data));
+  }
+
+  filter(key, cls, filter) {
+    let list = this.get(key, cls);
+
+    localStorage.setItem(key, JSON.stringify(list.filter(filter)));
+  }
+}
+
+export const storage = new Storage();
